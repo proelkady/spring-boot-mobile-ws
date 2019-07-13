bc4fcada426bcd7908ec83556fdfe60cbf30976a
Mohamed Elkady <pro.elkady@gmail.com>
2019-07-13 23:52:33 +0300

    + add app context path

--- src/main/java/com/proelkady/app/ws/MobileAppWsApplication.java
+++ src/main/java/com/proelkady/app/ws/MobileAppWsApplication.java
@@ -2,11 +2,13 @@ package com.proelkady.app.ws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class MobileAppWsApplication {
+public class MobileAppWsApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(MobileAppWsApplication.class, args);
@@ -20,6 +22,11 @@ public class MobileAppWsApplication {
 	@Bean
 	public SpringApplicationContext springApplicationContext() {
 		return new SpringApplicationContext();
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(MobileAppWsApplication.class);
 	}
 
 }
--- src/main/java/com/proelkady/app/ws/security/SecurityConstants.java
+++ src/main/java/com/proelkady/app/ws/security/SecurityConstants.java
@@ -6,7 +6,7 @@ public class SecurityConstants {
 	public static final long EXPIERATION_DATE = 864000000; // 10 days in milliseconds
 	public static final String TOKEN_PREFIX = "Bearer ";
 	public static final String HEADER_STRING = "Authorization";
-	public static final String SIGN_UP_URL = "/users";
+	public static final String SIGN_UP_URL = "mobile-app-ws/users";
 	
 	public static String tokenSecret() {
 		AppProperties props = (AppProperties) SpringApplicationContext.getBean("appProperties");
